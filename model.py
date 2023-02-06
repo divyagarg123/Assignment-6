ff944eaca445172e23f0a4216f5a9d8fc0d6524c
divyagarg123 <109232157+divyagarg123@users.noreply.github.com>
2023-02-06 23:30:52 +0530

    Update model.py

--- model.py
+++ model.py
@@ -7,32 +7,31 @@ class Model(nn.Module):
         super(Model, self).__init__()
 
         self.convblock1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), padding=1, stride=1, bias=False),   # r_in = 1, jin = 1 jout=1*1 =1, k=3, rout = 1+(3-1)*1 = 3
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=0, stride=1, dilation=2, bias=False),# r_in = 3, jin = 1 jout=1*1 =1, k=5, rout = 3+(5-1)*1 = 7
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), padding=1, stride=2, bias=False),   # r_in = 1, jin = 1 jout=1*2 =2, k=3, rout = 1+(3-1)*1 = 3
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=0, stride=1, dilation=2, bias=False),# r_in = 3, jin = 2 jout=2*1 =2, k=5, rout = 3+(5-1)*2= 11
             nn.ReLU(),
             nn.Dropout(0.05),
             nn.BatchNorm2d(64)
         )
         self.convblock2 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), padding=0, stride=1, bias=False),
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(1, 1), padding=0, stride=1, bias=False),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), padding=0, stride=2, bias=False), # r_in = 11, jin = 2 jout=2*2 =4, k=3, rout = 11+(3-1)*2 = 15
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(1, 1), padding=0, stride=1, bias=False),# r_in = 15, jin = 4 jout=4*1 =4, k=3, rout = 15+(3-1)*4 = 23
             nn.ReLU(),
             nn.Dropout(0.05),
             nn.BatchNorm2d(128)
         )
         self.convblock3 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding=0, stride=1, bias=False),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding=0, stride=2, bias=False),# r_in = 23, jin = 4 jout=4*2 =8, k=3, rout = 23+(3-1)*4 = 31
             # r_in = 21, jin = 8 jout=8*1 =8, k=1, rout = 5+(1-1)*8 = 21
             nn.ReLU(),
             nn.Dropout(0.05),
             nn.BatchNorm2d(128)
         )
         self.convblock4 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(1, 1), padding=0, stride=1, bias=False),
-            # r_in = 21, jin = 8 jout=8*2 =8, k=3, rout = 21+(3-1)*8 = 37
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(1, 1), padding=0, stride=1, bias=False),# r_in = 31, jin = 8 jout=1*4 =8, k=1, rout = 31+(1-1)*1 = 21
         )
         self.gap = nn.Sequential(
-            nn.AvgPool2d(kernel_size=24))  # r_in = 37, jin = 8,s=2, jout=8*2 =16, k=2, rout = 37+(2-1)*8 = 45
+            nn.AvgPool2d(kernel_size=3))  # r_in = 31, jin = 8,s=3, jout=1*8 =8, k=3, rout = 31+(3-1)8 = 47
         self.fc1 = nn.Linear(256, 64)
         self.fc2 = nn.Linear(64, 10)
 
